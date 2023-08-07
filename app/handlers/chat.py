0dfb88cca9b9ec2d951b967b064f1cf5505471ca
liubo <liubo@mokahr.com>
2023-08-07 20:11:01 +0800

    add_ai_contact

--- app/handlers/chat.py
+++ app/handlers/chat.py
@@ -19,3 +19,9 @@ async def activate_box():
     box_id = request.args.get("box_id")
     # change box id
     return {'status': 1, 'message': '修改成功'}
+
+
+@doctor_bp.route('ai_contact', methods=['GET'])
+async def ai_contact():
+    # ignore
+    return {'status':1, 'message':"test"}
--- utils/gen_exceptions.py
+++ utils/gen_exceptions.py
@@ -1,5 +1,6 @@
 class GetProxyError(Exception):
     """获取代理报错"""
 
+
 class RequestError(Exception):
     """请求报错"""
