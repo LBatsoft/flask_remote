2583b777a31b5775943d51dacb14ada1436ccea9
liubo235 <liubo235@ke.com>
2024-08-05 11:01:07 +0800

    xx

--- utils/xhs_decrpty.py
+++ utils/xhs_decrpty.py
@@ -3,3 +3,7 @@ import json
 
 def beauty_print(data: dict):
     print(json.dumps(data, ensure_ascii=False, indent=2))
+
+
+def beauty_json(data: dict):
+    return json.dumps(data, ensure_ascii=False, indent=2)
